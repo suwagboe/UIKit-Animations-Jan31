6d296b49f06d1b86f28b49957c7c76cb2d01cabe
uwagboe <sm.uwagboe@gmail.com>
2020-01-31 11:45:09 -0500

    made the pulsatingAnimation and scaleAnimation function to see the different ways that the images in a view controller can animate

--- UIKit-Animations-Jan31/view controllers/SampleAnimationsViewController.swift
+++ UIKit-Animations-Jan31/view controllers/SampleAnimationsViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class SampleAnimationsViewController: UIViewController {
+    /*
+Properties that can be animated
+     alpha
+     corner radius
+     position
+     center
+     transition
+     color
+     shadow
+     opacity
+     transfor: scale, rotate, translate
+     */
     
-    private let sampleAnimationsView = sampleAnimationView()
+    private let sampleAnimationsViewInstance = sampleAnimationView()
     
     override func loadView() {
-        view = sampleAnimationsView
+        view = sampleAnimationsViewInstance
     }
 
     override func viewDidLoad() {
@@ -23,9 +35,33 @@ class SampleAnimationsViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
+        //scaleAnimation()
+        pulsatingAnimation()
     }
     
+    private func pulsatingAnimation(){
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.repeat, .autoreverse, .curveLinear], animations: {
+            // animation block
+            self.sampleAnimationsViewInstance.pursuitLogo.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) { (done) in
+            // code to be excuted after animation is complete
+            // options: -can reset views values
+            //          -create another animation
+            // NB: if you want to go beyond vreating an animation in this completion handler the better choice would be animateKeyFranes()
+            UIView.animate(withDuration: 0.3) {
+                self.sampleAnimationsViewInstance.pursuitLogo.transform = CGAffineTransform.identity // restores it back to its original state
+            }
+        }
+    }
     
+    private func scaleAnimation(){
+        UIView.animate(withDuration: 1.5) { // this is a 3rd of a second
+            // any transform value of 1.0 represents the identity of the view..
+            self.sampleAnimationsViewInstance.pursuitLogo.transform = CGAffineTransform(scaleX: 20.0, y: 20.0)
+            self.sampleAnimationsViewInstance.pursuitLogo.alpha = 0.0
+            //self.sampleAnimationsViewInstance.pursuitLogo.transform3D = CATransform3D(0.9)
+        }
+    }
     
 
     
