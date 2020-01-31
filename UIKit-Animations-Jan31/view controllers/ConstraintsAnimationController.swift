070e25a01ccffb7b8546a546641426b94e91246f
uwagboe <sm.uwagboe@gmail.com>
2020-01-31 12:58:15 -0500

    connected the y constraint and the two buttons and gave it that values necessary for the view to move up and down

--- UIKit-Animations-Jan31/view controllers/ConstraintsAnimationController.swift
+++ UIKit-Animations-Jan31/view controllers/ConstraintsAnimationController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ConstraintsAnimationController: UIViewController {
 
+    // added the view constraint within the controller in order to gain access to it.
+    @IBOutlet weak var viewYConstraint: NSLayoutConstraint!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
         
     }
-    
 
+    
+    @IBAction func animateUpButton(_ sender: UIButton) {
+        // we want it to up so inside of here it needs to be a negative value..
+        
+        viewYConstraint.constant -= 100
+        // plus 100 .. removing 100 from the constraint
+    }
+    
+    
+    
+    @IBAction func animateDownButton(_ sender: UIButton) {
+        // to go down you need a positive value
+        
+        viewYConstraint.constant += 100
+        // plus 100 .. adding 100
+        
+    }
+    
     
 
 }
