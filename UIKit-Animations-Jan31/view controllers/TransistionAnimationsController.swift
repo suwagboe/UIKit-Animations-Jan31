2319c6e101a03a58eca1607203f67b772619904b
uwagboe <sm.uwagboe@gmail.com>
2020-02-04 11:20:27 -0500

    made the function animate, and added a tap gesture for the function. needed to enable the user interaction on the image View.  imageView.addGestureRecognizer(tapGesture) tells the image view to recognizer that the image is getting tapped

--- UIKit-Animations-Jan31/view controllers/TransistionAnimationsController.swift
+++ UIKit-Animations-Jan31/view controllers/TransistionAnimationsController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class TransistionAnimationsController: UIViewController {
     
+    //create a tap gesture
+    private lazy var tapGesture: UITapGestureRecognizer = {
+        // this only declares the gesture. but it doesn't me it gets used this just says that it is here.
+        let gesture = UITapGestureRecognizer()
+        // the image cannot respond to the function. so it needs to be on the image. the target is where we are dragging it to
+        gesture.addTarget(self, action: #selector(animate))
+        
+        return gesture
+    }()
     
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // set default image
+        imageView.image = UIImage(named: "dog")
+        view.backgroundColor = .systemPink
+        
+        // need to enable user interation on image view or a label as those two UI element are set to false by default
+        imageView.isUserInteractionEnabled = true
+        
+        // add gesture to the image view this part enable the interation between the gesture and the image
+        imageView.addGestureRecognizer(tapGesture) // this is the gesture it should recognizer.
     }
 
+    @objc
+    private func animate() {
+        // set up gesture that when the image is tap the image gets called.
+        print("animate") // to confirm that this does get called when it is tapped
+        
+        
+        
+    }
+    
 }
